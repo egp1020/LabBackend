5ec23c18487d8508d6f53a63bcdf941dc7a9235e
egp1020 <egp1020.developer@gmail.com>
2024-10-12 05:08:32 -0500

    feat: adding video-views structures. egp1020.

---
+++ src/main/java/culturemedia/model/View.java
@@ -0,0 +1,10 @@
+package culturemedia.model;
+
+import java.time.LocalDateTime;
+
+public record View (
+        String userFullName,
+        LocalDateTime startPlayingTime,
+        Integer age,
+        Video video){
+}
