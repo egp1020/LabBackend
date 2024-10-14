5d729e983632860691d7ce7a9243fcd2a79bcf1a
egp1020 <egp1020.developer@gmail.com>
2024-10-13 21:11:11 -0500

    feat: adding exceptions structures. egp1020.

--- src/main/java/culturemedia/exception/VideoNotFoundException.java
+++ src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -2,6 +2,11 @@ package culturemedia.exception;
 
 
 public class VideoNotFoundException extends CultureMediaException{
+
+    public VideoNotFoundException() {
+        super("Video not found");
+    }
+
     public VideoNotFoundException(String title) {
         super("Video not found: " + title);
     }
